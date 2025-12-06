39dea7e6e3c53c0ee2df424a6b0dc1c3d1ec690b
Taxi Shayak <taxisahayak@gmail.com>
2025-12-06 13:23:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1399740422-cf8fe",
+  "appId": "1:945512632724:web:9fd6412ce6f352c38bff81",
+  "apiKey": "AIzaSyAndinLxMY24F9xd2OOC_LY-1YsP3izHOA",
+  "authDomain": "studio-1399740422-cf8fe.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "945512632724"
+};
